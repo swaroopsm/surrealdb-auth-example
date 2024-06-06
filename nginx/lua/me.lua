a149b545344e735637ae6e826de2e757673fc4e0
Swaroop Sethumadhavan <swaroop.striker@gmail.com>
2024-06-06 09:01:31 +0200

    Handle 401 for for /me

--- nginx/lua/me.lua
+++ nginx/lua/me.lua
@@ -2,12 +2,17 @@ local surrealdb = require("surrealdb")
 local cjson = require("cjson")
 
 local response = surrealdb.fn("whoami")
-local data = cjson.decode(response.body)
-ngx.log(ngx.INFO, data[1].status)
 
-if data[1].status == "ERR" then
-	return ngx.exit(ngx.HTTP_UNAUTHORIZED)
+if response.status == ngx.HTTP_OK then
+	local data = cjson.decode(response.body)
+	ngx.log(ngx.INFO, data[1].status)
+
+	if data[1].status == "ERR" then
+		return ngx.exit(ngx.HTTP_UNAUTHORIZED)
+	end
+
+	ngx.say(cjson.encode(data[1].result))
+	ngx.exit(ngx.OK)
+else
+	ngx.exit(response.status)
 end
-
-ngx.say(cjson.encode(data[1].result))
-ngx.exit(ngx.OK)
