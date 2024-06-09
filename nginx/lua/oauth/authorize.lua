a13823a9d40c3bb4dbda94ea25084d682430e433
Swaroop Sethumadhavan <swaroop.striker@gmail.com>
2024-06-09 20:58:32 +0200

    Reset auth_token session if its invalid

--- nginx/lua/oauth/authorize.lua
+++ nginx/lua/oauth/authorize.lua
@@ -1,11 +1,33 @@
+local ck = require("resty.cookie")
 local surrealdb = require("surrealdb")
 local cjson = require("cjson")
+local cookie = ck:new()
 
 if ngx.var[1] == "github" or ngx.var[1] == "google" then
 	local response = surrealdb.fn(ngx.var[1] .. "__oauthUrl")
-	local data = cjson.decode(response.body)
 
-	return ngx.redirect(data[1].result, 302)
+	if response.status == 200 then
+		local data = cjson.decode(response.body)
+		local data = cjson.decode(response.body)
+		return ngx.redirect(data[1].result, 302)
+	else
+		-- Check if authToken exists and is invalid
+		-- Then we reset the cookie and try to hit this route again
+		local authToken = cookie:get("__auth_token")
+
+		if authToken then
+			cookie:set({
+				key = "__auth_token",
+				value = "",
+				path = "/",
+			})
+
+			-- TODO: Don't hardcode FE URL here
+			ngx.redirect(os.getenv("FRONTEND_URL") .. "/api/oauth/" .. ngx.var[1], 302)
+		end
+
+		ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+	end
 else
 	ngx.exit(ngx.HTTP_BAD_REQUEST)
 end
